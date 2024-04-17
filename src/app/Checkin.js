5d284358180caf1e12ede3ce1845b99aeffc5903
AsadullahAbbasi <asadullahabbasi225@gmail.com>
2024-04-17 22:24:12 +0500

    a

---
+++ src/app/Checkin.js
@@ -0,0 +1,18 @@
+"use client"
+import React from 'react'
+import CloseIcon from '@mui/icons-material/Close';
+const Checkin = () => {
+  return (
+   <section>
+      <div className='max-w-[572px]' >
+        <div className='flex justify-between gap-44 bg-[#F8F8F8] roun'>
+        <p>Add Check in</p>
+        <CloseIcon/>
+
+        </div>
+      </div>
+   </section>
+  )
+}
+
+export default Checkin
--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,12 @@
-import React from 'react';
+import React from 'react'
+import Checkin from './Checkin'
 
-function ImageDragAndDrop() {
+const page = () => {
   return (
     <div>
-      <div
-        id="drop-area"
-        onDrop={onDrop}
-        onDragOver={onDragOver}
-        style={{
-          width: '300px',
-          height: '300px',
-          border: '2px dashed gray',
-          borderRadius: '10px',
-          textAlign: 'center',
-          padding: '20px',
-        }}
-      >
-        Drop Image Here
-      </div>
-      <img
-        id="drag-image"
-        src="https://via.placeholder.com/150"
-        alt="Draggable Image"
-        draggable="true"
-        onDragStart={onDragStart}
-        style={{ width: '150px', marginTop: '20px' }}
-      />
+      <Checkin/>
     </div>
-  );
+  )
 }
 
-// Define event handlers outside of the component
-function onDragStart(e) {
-  e.dataTransfer.setData("text/plain", e.target.id);
-}
-
-function onDrop(e) {
-  e.preventDefault();
-  const data = e.dataTransfer.getData("text/plain");
-  const draggableElement = document.getElementById(data);
-  e.target.appendChild(draggableElement);
-}
-
-function onDragOver(e) {
-  e.preventDefault();
-}
-
-export default ImageDragAndDrop;
+export default page
